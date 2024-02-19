21c82388a2b495769c2ded388696928755305e71
Iggi Jimenez <89427749+iggijimenez@users.noreply.github.com>
2024-02-19 13:30:40 -0800

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by jimenez on 2/19/24.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
